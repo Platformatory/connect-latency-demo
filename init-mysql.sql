5f73b016111e026c3946d2124ee014932a2a037f
p6 <pavan@grahana.net>
2023-09-18 18:23:19 +0530

    separate init scripts for both DBs

---
+++ init-mysql.sql
@@ -0,0 +1,22 @@
+-- Create "users" table
+CREATE TABLE `jdbc-source-users` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    source_time TIMESTAMP,
+    sink_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    username VARCHAR(50),
+    email VARCHAR(255),
+    age INT
+);
+
+-- Create "orders" table
+CREATE TABLE `jdbc-source-orders` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    source_time TIMESTAMP,
+    sink_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_id INT,
+    total_price DECIMAL(10, 2),
+    status VARCHAR(50),
+);
+
--- init-postgres.sql
+++ init-postgres.sql
@@ -2,6 +2,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    source_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     username VARCHAR(50),
     email VARCHAR(255),
     age INT
@@ -11,6 +12,7 @@ CREATE TABLE users (
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    source_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id INT,
     total_price DECIMAL(10, 2),
     status VARCHAR(50)
