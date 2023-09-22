0be1b3e6f859efc7e8466fe8c93d8e49a375c925
Ashwin Venkatesan <ashwin@platformatory.com>
2023-09-22 18:42:50 +0530

    Create schema for telemetry topic explicitly

--- setup_kafka_cluster.sh
+++ setup_kafka_cluster.sh
@@ -7,4 +7,6 @@ sasl.jaas.config=$CONNECT_SASL_JAAS_CONFIG
 security.protocol=SASL_SSL
 sasl.mechanism=PLAIN"
 
-echo "$CONFIG_STRING" | kafka-topics --bootstrap-server $BOOTSTRAP_SERVERS --create --if-not-exists  --topic connect_latency_telemetry --partitions 6  --command-config /dev/stdin
+echo "$CONFIG_STRING" | kafka-topics --bootstrap-server $BOOTSTRAP_SERVERS --create --if-not-exists  --topic test_topic --partitions 6  --command-config /dev/stdin
+
+curl -X POST $CONNECT_VALUE_CONVERTER_SCHEMA_REGISTRY_URL/subjects/test_topic-value/versions\?normalize=false -u $CONNECT_VALUE_CONVERTER_SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO -H 'Content-Type: application/vnd.schemaregistry.v1+json' -d '@schema-connect_latency_telemetry-value.json'
