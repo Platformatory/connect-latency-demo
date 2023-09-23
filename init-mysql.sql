9dfb3d0ad80277acec00a8711d64f87f1e79773a
p6 <pavan@grahana.net>
2023-09-23 17:15:33 +0530

    first line working version with CDC conector

--- init-mysql.sql
+++ init-mysql.sql
@@ -20,3 +20,23 @@ CREATE TABLE `jdbc-source-orders` (
     status VARCHAR(50)
 );
 
+CREATE TABLE `debzcdc_public_users` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    source_time TIMESTAMP,
+    sink_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    username VARCHAR(50),
+    email VARCHAR(255),
+    age INT
+);
+
+-- Create "orders" table
+CREATE TABLE `debzcdc_public_orders` (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    source_time TIMESTAMP,
+    sink_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    user_id INT,
+    total_price DECIMAL(10, 2),
+    status VARCHAR(50)
+);
---
+++ scripts/create_jdbc_mysql_sink_cdc.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+source .env
+
+CONNECT_REST_API="http://localhost:8083/connectors" 
+CONNECTOR_NAME="cdc-jdbc-sink-connector" 
+JDBC_URL="jdbc:mysql://mysql:3306/mysql_db"
+DB_USER="mysql_user"
+DB_PASSWORD="mysql_password"
+TOPICS_REGEX="debzcdc\\\\.public\\\\..*"
+
+PAYLOAD=$(cat <<EOF
+{
+  "name": "$CONNECTOR_NAME",
+  "config": {
+    "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+    "tasks.max": "1",
+    "connection.url": "$JDBC_URL",
+    "connection.user": "$DB_USER",
+    "connection.password": "$DB_PASSWORD",
+    "topics.regex": "$TOPICS_REGEX",
+    "auto.create": "true",
+    "insert.mode": "upsert",
+    "transforms": "unwrap,reroute",
+    "transforms.unwrap.type": "io.debezium.transforms.ExtractNewRecordState",
+    "transforms.unwrap.drop.tombstones": "false",
+    "transforms.unwrap.delete.handling.mode": "none",
+    "transforms.reroute.type": "org.apache.kafka.connect.transforms.RegexRouter",
+    "transforms.reroute.regex": "debzcdc\\\\.public\\\\.(.+)",
+    "transforms.reroute.replacement": "debzcdc_public_\$1",
+    "consumer.override.connect.pipeline.id": "CDCPipeline",
+    "consumer.override.telemetry.type": "sink"
+  }
+}
+EOF
+)
+
+curl -s -X POST -H "Content-Type: application/json" --data "$PAYLOAD" $CONNECT_REST_API
+
+if [ $? -eq 0 ]; then
+  echo "Connector created successfully!"
+else
+  echo "Failed to create connector!"
+  exit 1
+fi
